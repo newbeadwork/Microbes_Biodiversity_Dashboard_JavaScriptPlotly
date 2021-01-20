f1e996b5406fb3f8a43e142f29ec52a156ee6158
Nataliia Sokolova <newbeadwork@gmail.com>
2021-01-19 22:39:12 -0800

    Bar chart upgrade and additional variations app1

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,15 +1,15 @@
 
-d3.json("samples.json").then((data) => {
-
+/*d3.json("samples.json").then((data) => {
 
         data.names.forEach(name => {
                 d3.select("#selDataset").append("option").text(name);
         })
-})
-d3.selectAll("#selDataset").on("change", optionChanged);
-
+})*/
 function init() {
         d3.json("samples.json").then((data) => {
+                data.names.forEach(name => {
+                        d3.select("#selDataset").append("option").text(name);
+                });
                 var board = d3.select("#sample-metadata");
 
                 board.html("");
@@ -58,112 +58,88 @@ function init() {
                 Plotly.newPlot("bar", data, layout);
         }); 
 }
+d3.selectAll("#selDataset").on("change", optionChanged);
+
+
 
 function optionChanged() {
 
         var dataset = d3.select(this).property("value");
-
-        buildPlot(dataset);
-        buildTable(dataset);
-        buildBubble(dataset);
-}
-
-
-function buildPlot(dataset) {
         d3.json("samples.json").then((data) => {
-
-                var filteredData = data.samples.filter(x => x.id === dataset);
-                filteredData.sort(function compareFunction(firstObj, secondObj) {
-                        return firstObj.sample_values - secondObj.sample_values;
-                });
-
-                var microbesID = filteredData.map(x => x.otu_ids.slice(0, 10));
-                var microbesSpiecies = filteredData.map(x => x.otu_labels.slice(0, 10));
-                var microbesValues = filteredData.map(x => x.sample_values.slice(0, 10));
-                console.log(filteredData);
-                console.log(microbesID[0]);
-                console.log(microbesSpiecies[0]);
-                console.log(microbesValues[0]);
-
-
-                var trace1 = {
-                        x: microbesValues[0],
-                        y: microbesID[0],
-                        text: microbesSpiecies[0],
-                        name: "Something",
-                        type: "bar",
-                        orientation: "h"
-                };
-
-                var data = [trace1];
-
-                var layout = {
-                        title: "OTUs found in that individual",
-
-                };
-
-                Plotly.newPlot("bar", data, layout);
+            var filteredData = data.samples.filter(x => x.id === dataset);
+            filteredData.sort(function compareFunction(firstObj, secondObj) {
+                return firstObj.sample_values - secondObj.sample_values;
+            });
+            var chosenMetadata = data.metadata.filter(x => x.id == dataset)[0];
+            buildPlot(filteredData);
+            buildTable(chosenMetadata);
         });
-}
-
-function buildTable(dataset) {
-
-
-        var board = d3.select("#sample-metadata");
-
-        board.html("");
-
-        d3.json("samples.json").then((data) => {
-
-                var chosenMetadata = data.metadata.filter(x => x.id == dataset);
-                console.log(chosenMetadata);
-
-                Object.entries(chosenMetadata[0]).forEach(([key, value]) => {
-                        board.append("h4").text(`${key}: ${value}`);
-                        
-                });
-
+    
+    }
+    
+    function buildPlot(filteredData) {
+    
+    
+        var microbesID = filteredData.map(x => x.otu_ids)[0];
+        var microbesSpiecies = filteredData.map(x => x.otu_labels)[0];
+        var microbesValues = filteredData.map(x => x.sample_values)[0];
+       console.log(microbesID);
+        var microbesIDtop10 = microbesID.slice(0, 10);
+        var microbesSpieciesTop10 = microbesSpiecies.slice(0, 10);
+        var microbesValuesTop10 = microbesValues.slice(0, 10);
+        console.log(microbesValuesTop10);
+        var names = [];
+        microbesIDtop10.forEach(x => {
+                 names.push(`OTU ${x}`);
         })
-}
-function buildBubble(dataset) {
-        d3.json("samples.json").then((data) => {
-
-                var filteredData = data.samples.filter(x => x.id === dataset);
-
-                var microbesID = filteredData.map(x => x.otu_ids);
-                var microbesSpiecies = filteredData.map(x => x.otu_labels);
-                var microbesValues = filteredData.map(x => x.sample_values);
-                console.log(filteredData);
-                console.log(microbesID[0]);
-                console.log(microbesSpiecies[0]);
-                console.log(microbesValues[0]);
-                //microbesID.forEach(console.log(`OTU ${microbesID[0]}`));
-
-                var trace1 = {
-                        x: microbesID[0],
-                        y: microbesValues[0],
-                        mode: 'markers',
-                        marker: {
-                                size: microbesValues[0]
-                        }
-                };
-
-
-                var data = [trace1];
-
-
-                var layout = {
-                        title: "top 10 OTUs found in that individual",
-
-                };
-
-                Plotly.newPlot("bubble", data, layout);
-
-
+        var dataBar = [{
+    
+            x: microbesValuesTop10,
+            y: names,
+            text: microbesSpieciesTop10,
+            name: "Something",
+            type: "bar",
+            orientation: "h"
+    
+        }];
+    
+        var layoutBar = {
+            title: "top 10 OTUs found in that individual",
+    
+        };
+    
+        Plotly.newPlot("bar", dataBar, layoutBar);
+    
+        var dataBubble = [{
+            x: microbesValues,
+            y: microbesID,
+            text: microbesSpiecies,
+            mode: 'markers',
+            marker: {
+                size: microbesValues
+            }
+        }];
+    
+        var layoutBubble = {
+            title: "OTUs found in that individual",
+    
+        };
+    
+        Plotly.newPlot("bubble", dataBubble, layoutBubble);
+    
+    }
+    
+    function buildTable(chosenMetadata) {
+    
+        var board = d3.select("#sample-metadata");
+    
+        board.html("");
+    
+        Object.entries(chosenMetadata).forEach(([key, value]) => {
+            board.append("h4").text(`${key}: ${value}`);
+    
         });
-
-
-
-
-}
-init();
+    
+    
+    }
+    init();
