076dc3e56365d182fb1711d24197054038e53175
Nataliia Sokolova <newbeadwork@gmail.com>
2021-01-19 16:29:19 -0800

    Filtering and slicing by ID

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,42 +1,52 @@
 
 
-function buildPlot() {
+d3.json("samples.json").then((data) => {
 
 
-        d3.json("samples.json").then((data) => {
-                console.log(data);
-                data.samples.sort(function compareFunction(firstObj, secondObj) {
+        data.names.forEach(name => {
+                d3.select("#selDataset").append("option").text(name)
+        });
+
+
+        function init() {
+
+                var filteredData = data.samples.filter(x => x.id === "940");
+                console.log(filteredData);
+        }
+
+        d3.selectAll("#selDataset").on("change", function () {
+                //var dropdownMenu = d3.select("#selDataset");
+                // Assign the value of the dropdown menu option to a variable
+                var dataset = d3.select(this).property("value");
+                console.log(dataset);
+                var filteredData = data.samples.filter(x => x.id === dataset);
+                filteredData.sort(function compareFunction(firstObj, secondObj) {
                         return firstObj.sample_values - secondObj.sample_values;
                 });
-                console.log(data.samples);
-                var participantID = data.samples.map(x => x.id);
-                var microbesID = data.samples.map(x => x.otu_ids.slice(0, 10));
-                var microbesSpiecies = data.samples.map(x => x.otu_labels.slice(0, 10));
-                var microbesValues = data.samples.map(x => x.sample_values.slice(0, 10));
-                console.log(participantID)
+
+                var microbesID = filteredData.map(x => x.otu_ids.slice(0, 10));
+                var microbesSpiecies = filteredData.map(x => x.otu_labels.slice(0, 10));
+                var microbesValues = filteredData.map(x => x.sample_values.slice(0, 10));
+                console.log(filteredData);
                 console.log(microbesID);
                 console.log(microbesSpiecies);
                 console.log(microbesValues);
 
 
-                //Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
 
-                //Use sample_values as the values for the bar chart.
-
-                //Use otu_ids as the labels for the bar chart.
-
-                //Use otu_labels as the hovertext for the chart.
-
-                var selectDropdown = d3.select("#selDataset")
+        });
 
 
-
-                participantID.forEach(name => {
-                        var appendOption = selectDropdown.append("option").text(name);
-                })
-
-
-
+        init();
+        /*function handleChange() {
+          // Prevent the page from refreshing
+          d3.event.preventDefault();
+        
+          
+          buildPlot(id);
+        }
+        
+        function buildPlot(id) {
                 var trace1 = {
                         x: microbesValues,
                         y: microbesID,
@@ -45,20 +55,23 @@ function buildPlot() {
                         type: "bar",
                         orientation: "h"
                 };
-
-
+ 
+ 
                 var data = [trace1];
-
-
+ 
+ 
                 var layout = {
                         title: "top 10 OTUs found in that individual",
-
+ 
                 };
+ 
+                Plotly.newPlot("bar", data, layout); 
+        }
 
-                Plotly.newPlot("bar", data, layout);
-        });
+       */
+});
 
-}
 
-buildPlot();
+
+
 
